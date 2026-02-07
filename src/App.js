7b8937dabde8e2e2615e7bd6e3ae0acfe1d3d44c
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-02-07 16:04:31 +0530

    start refactoring

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
 import './App.css';
 import React from 'react';
+import FirstComponent from './component/Firstcomponent';
 
 function App() {
   return (
     <div className="App">
       <h1>Nadeem Anwar Todo Application</h1>
       <FirstComponent />
-      <SecondComponent />
-      <ThirdComponent />
     </div>
   );
 }
 
-//creating our first component
-function FirstComponent() {
-  return (
-    <div>
-      <h1>This is my first component</h1>
-    </div>
-  );
-}
-
-function SecondComponent() {
-  return (
-    <div>
-      <h1>This is my second component</h1>
-    </div>
-  );
-} 
 
 
-
-//creating first class component
-class ThirdComponent extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>This is my third class component</h1>
-      </div>
-    );
-  }
-}
 
 
 export default App;
---
+++ src/component/Firstcomponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FirstComponent() {
+  return (
+    <div>
+      <h1>This is my first component from compoent folder</h1>
+    </div>
+  );
+}
+
+export default FirstComponent;
