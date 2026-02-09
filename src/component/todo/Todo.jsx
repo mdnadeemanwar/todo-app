446eb342add4e75f6a1a81331915cf0e5665c2a2
mdnadeemanwar <anwarnadeem2010@gmail.com>
2026-02-09 21:24:19 +0530

    Refactoring is done

--- src/component/todo/Todo.jsx
+++ src/component/todo/Todo.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 // import './Todo.css';
-import { BrowserRouter, Routes, useNavigate, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "../Welcome";
 import ErrorComponent from "../ErrorComponent";
 import Todos from "../Todos";
 import HeaderComponent from "../HeaderComponent";
 import FooterComponent from "../FooterComponent";
 import LogoutComponent from "../LogoutComponent";
+import LoginComponent from "../LoginComponent";
 
 function Todo() {
   return (
@@ -26,73 +27,6 @@ function Todo() {
   );
 }
 
-function LoginComponent() {
-  const [showSuccess, setShowSuccess] = useState(false);
-  const [showError, setShowError] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
 
-  const navigate = useNavigate();
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (username === "admin" && password === "password") {
-      setShowSuccess(true);
-      setShowError(false);
-      setUsername("");
-      setPassword("");
-      navigate(`/welcome/${username}`);
-    } else {
-      setShowError(true);
-      setShowSuccess(false);
-    }
-  }
-
-  function SuccessMessage() {
-    return <div>Authentication successful</div>;
-  }
-
-  function FailureMessage() {
-    return (
-      <div>Authentication failed !! , Please enter correct credential</div>
-    );
-  }
-
-  return (
-    <div>
-      {showSuccess && <SuccessMessage />}
-      {showError && <FailureMessage />}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Username:
-            <input
-              name="username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password:
-            <input
-              name="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  );
-}
 
 export default Todo;
